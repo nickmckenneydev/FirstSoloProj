68d4af9ca537afda449a8be0484e72d054c97c1a
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-12-04 19:56:42 -0500

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ GLfloat vertices[] = {
     0.0f,0.5f,0.0f
 
 };
-Not working
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and 
@@ -64,50 +64,3 @@ return 0;
 //graphics pipeline is 2 parts
 //transform 3d coord into 2d coord
 //2d coord into color pixels// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);//Converts normal coords to screen coords
-}
-void processInput(GLFWwindow *window){
-if(glfwGetKey(window,GLFW_KEY_ESCAPE)==GLFW_PRESS){
-    glfwSetWindowShouldClose(window, true);
-}
-}
-int main()
-{
-  glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-   GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
- if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }    
-    while (!glfwWindowShouldClose(window)){//Render Loop. Each time its one frame
-        processInput(window);
-            glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(1.0f,0.0f,0.0f,1.0f);
-    
- glfwSwapBuffers(window);//This is double buffering. Swapping front buffer and back. Allows to avoid not seeing pixel by pixel drawing
-    glfwPollEvents();//any events are triggered. Like keyboard or mouse
-    }
-
-glfwTerminate();
-return 0;
-}
