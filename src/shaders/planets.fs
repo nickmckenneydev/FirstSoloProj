c19bb92f4f244c87d864b9b827bc330ccc28da90
nickmckenneydev <nick.mckenney.dev@gmail.com>
2026-01-13 22:28:34 -0500

    test

--- src/shaders/planets.fs
+++ src/shaders/planets.fs
@@ -45,7 +45,7 @@ void main()
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 result = CalcDirLight(dirLight, norm, viewDir);
     // for(int i = 0; i < NR_POINT_LIGHTS; i++)
-    //     result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);   
+    //     result += CalcPointLight(pointLights[i], norm, FragPos,  viewDir);   
     
   FragColor = vec4(result.x,result.y,result.z,1.0);
 
